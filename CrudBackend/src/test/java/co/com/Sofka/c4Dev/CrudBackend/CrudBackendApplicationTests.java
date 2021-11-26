8fea0804a8ba49cbbf29ee3a2b4be5c26f15606d
Dayro Martinez <djmd96@hotmail.com>
2021-11-26 01:47:43 -0500

    Plantilla general configuracion Backend Spring Boot

---
+++ CrudBackend/src/test/java/co/com/Sofka/c4Dev/CrudBackend/CrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.Sofka.c4Dev.CrudBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
